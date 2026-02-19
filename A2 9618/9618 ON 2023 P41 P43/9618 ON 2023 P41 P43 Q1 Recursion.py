52618a745f9211d11f5c58b511507c751ec60647
Fazal Mohani <fazalmohani1@gmail.com>
2026-02-19 20:51:46 +0400

    Fixed function calls

--- A2 9618/9618 ON 2023 P41 P43/9618 ON 2023 P41 P43 Q1 Recursion.py
+++ A2 9618/9618 ON 2023 P41 P43/9618 ON 2023 P41 P43 Q1 Recursion.py
@@ -9,15 +9,15 @@ def RecursiveVowels(Value):         #method 1 using slicing
     if len(Value) == 0:
         return 0
     if Value[0] in ['a','e','i','o','u']:
-        return 1 + newRecursiveVowels(Value[1:])
-    return 0 + newRecursiveVowels(Value[1:])
+        return 1 + RecursiveVowels(Value[1:])
+    return 0 + RecursiveVowels(Value[1:])
 
 def newRecursiveVowels(Value, x):   #method 2 using index
     if x == len(Value):
         return 0
     if Value[x] in ['a','e','i','o','u']:
-        return 1 + RecursiveVowels(Value, x + 1)
-    return 0 + RecursiveVowels(Value, x + 1)
+        return 1 + newRecursiveVowels(Value, x + 1)
+    return 0 + newRecursiveVowels(Value, x + 1)
 
 print(IterativeVowels("house"))
 print(RecursiveVowels("imagine", 0))
